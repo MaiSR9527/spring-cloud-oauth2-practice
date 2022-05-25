58ac1dcad38112192cc7cb33ef0790e3d7324b49
v_maishuren <maisrcn@qq.com>
2022-05-25 14:11:03 +0800

    use nacos replace to eureka

--- oauth2-server/src/main/java/com/msr/oauth2/OAuth2ServerApp.java
+++ oauth2-server/src/main/java/com/msr/oauth2/OAuth2ServerApp.java
@@ -1,22 +1,14 @@
-package com.msr.eurekaserver;
+package com.msr.oauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-/**
- * Eureka Server
- *
- * @author MaiShuRen
- * @site https://www.maishuren.top
- * @since 2022/05/23
- */
 @EnableDiscoveryClient
 @SpringBootApplication
-public class EurekaServerApplication {
+public class OAuth2ServerApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(OAuth2ServerApp.class, args);
     }
-
 }
